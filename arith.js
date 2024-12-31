3b77448ce19bad7f5fc2cf014f0ca9a7936f9488
Ramyasri-0501 <padilamramyasri16@gmail.com>
2024-12-31 14:37:57 +0530

    Add files via upload

---
+++ arith.js
@@ -0,0 +1,6 @@
+const name = require('./server1')
+console.log(name.addition(22, 4));
+console.log(name.subtraction(22, 4));
+console.log(name.multiplication(22, 4));
+console.log(name.division(22, 4));
+console.log(name.remainder(22, 4));
